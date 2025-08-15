647ed27fc5732705a1266d7e008dbcbedc2e0e7a
Stefan Yas <stefan.sy.yas@gmail.com>
2025-08-15 13:29:08 +0200

    chore: add the correct astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
-// @ts-check
 import {
     defineConfig
 } from 'astro/config';
-import node from "@astrojs/node";
-
-import vercel from "@astrojs/vercel";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
+    // Replace the node() adapter with the vercel() adapter
     adapter: vercel(),
 });
